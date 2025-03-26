d7e76c9c35e8fe2037f60ba39be3fff479b464df
Jose <jlarapr@applicapr.com>
2025-03-26 08:49:56 -0400

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using NetSparkleUpdater;
+using NetSparkleUpdater.AppCastHandlers;
+using NetSparkleUpdater.Downloaders;
+
+
+namespace NetSparkleUpdaterApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window {
+    public MainWindow() {
+        InitializeComponent();
+        CheckForUpdates();
+    }
+
+    private async void CheckForUpdates() {
+        string appcastUrl = "https://jlarapr.github.io/NetSparkleUpdaterApp/appcast.xml";
+
+        var sparkle = new SparkleUpdater(appcastUrl, null);
+
+        var updateInfo = await sparkle.CheckForUpdatesQuietly();
+        if (updateInfo.Status == NetSparkleUpdater.Enums.UpdateStatus.UpdateAvailable)
+        {
+            sparkle.ShowUpdateNeededUI(updateInfo.Updates, true);
+        }
+    }
+}
